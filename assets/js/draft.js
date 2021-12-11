e443252c53ccaaa20ae4f3717495107207c45674
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2021-12-10 22:28:46 -0600

    i broke something in generatePassword() or else it didn't work when I initally thought it did. starting fresh in the morning.

--- assets/js/draft.js
+++ assets/js/draft.js
@@ -3,6 +3,7 @@ var numChar = ['0','1','2','3','4','5','6','7','8','9'];
 var upperChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var specialChar = ['"','`','~','!','@','#','$','%','^','&','*',',','_','-','+','=','<','>','.','?','/',':',';', '/','|','(',')','{','}'];
+//var passwordLength = '1234567890';
 
 var allChar = numChar + upperChar + lowerChar + specialChar;
 
@@ -15,19 +16,33 @@ var allChar = numChar + upperChar + lowerChar + specialChar;
 //     randPasswordArray = randPasswordArray(allChar, 4);
 //   };
 
-// user is asked for initial input, please select password length.
-        var generatePassword = function(){
-            window.prompt("Please select a password length from 8 to 128 characters.");
-            if (password <= ) {
-                window.prompt('Your password must be 8 or more characters.');
-            } else if (password >= 128) {
-                window.prompt('You need to select a number less than 129 characters long.');
-            } else {
-                window.prompt('You did not enter a valid selection. Please try again.');
-            }
+// user is asked for initial input, please select password length. **did not wor after 10 p.m. I broke something.
+var generatePassword = function() {
+    window.prompt("Please select a password length from 8 to 128 characters.");
+        if (passwordLength <= 7) {
+            window.prompt('Your password must be 8 or more characters. Please try again.');
+        } else if (passwordLength >= 129) {
+            window.prompt('You need to select a number less than 129 characters long.');
+        } else {
+            window.prompt('You did not enter a valid selection. Please try again.')
+            generatePassword();
         };
+};
 
-
+// window.prompt('Please select at least one type of character for your password to generate. Please choose from: uppercase, lowercase, special, numeric, or all');
+    //     if (upperChar) {
+    //         password = upperChar[i].Math.random()* password.length;
+    //     } else if (lowerChar) {
+    //         password = lowerChar.Math.random()*password.length;
+    //     } else if (specialChar) {
+    //         password = specialChar.Math.random()*password.length;
+    //     } else if (numChar) {
+    //         password = numChar.Math.random()*password.length;
+    //     } else if (allChar) {
+    //         password = allChar.Math.random()*password.length;
+    //     } else {
+    //         window.alert('You need to select from the following: uppercase, lowercase, special, numeric, or all.')
+    //     };
 
     // for (let i = 0; i > password.length * 8 && i < password.length * 128; i++) {
     //     const randomPassword = randPasswordArray[i];
@@ -36,7 +51,7 @@ var allChar = numChar + upperChar + lowerChar + specialChar;
 
 // user is asked for secondary input, please select character type. User must select at least one for the generator to work.
 var selectType = function() {
-    window.prompt('You must select at least one type of character for your password to generate. Please choose from: uppercase, lowercase, special, numeric, or all');
+    window.prompt('Please select at least one type of character for your password to generate. Please choose from: uppercase, lowercase, special, numeric, or all');
     if (upperChar) {
         password = upperChar.Math.random()*password.length;
     } else if (lowerChar) {
